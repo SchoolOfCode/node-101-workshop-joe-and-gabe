38319481a3fbd24613d60dfb1216576568889eb1
jafcoder <joefarr113@gmail.com>
2021-12-06 16:56:06 +0000

    task 2c completed

--- index.js
+++ index.js
@@ -20,10 +20,13 @@ let myCollection = [
 // If there's only one of the item in your collection, it should log "I have a name. Here's what I like about it: whatILike". 
 // If you have more than one of it, the message should log "I have count names. Here's what I like about them: whatILike".
 
-function describeItem(){
-    let itemName = myCollection.name;
-    let whatILike = myCollection.whatILike
-    console.log("I have a " + itemName +". Here's what I like about it:" + whatILike +".")
-}
-
-describeItem([0]);
+function describeItem(item){
+  if (item.count === 1){ 
+    console.log("I have a" + item.name + ". Here's what I like about it:" + item.whatILike + ".")
+  } 
+  if (item.count > 1){
+      console.log("I have a " + item.name + ". Here's what I like about it: " + item.whatILike + ".")
+    }
+  }
+  
+describeItem(myCollection[2])
