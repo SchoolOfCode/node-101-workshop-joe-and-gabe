3ca9ea9152a407f13facf8971f50587ac0e87e66
jafcoder <joefarr113@gmail.com>
2021-12-06 17:25:29 +0000

    up to task 3 completed, collection.js file added and functions still run

--- index.js
+++ index.js
@@ -1,24 +1,9 @@
-let myCollection = [
-    {
-      name: "School of Code mug",
-      count: 1,
-      whatILike: "It has my cute pixel character on it!"
-    },
-    {
-      name: "School of Code hat",
-      count: 2,
-      whatILike: "An often overlooked fashion accessory"
-    },
-    {
-      name: "School of Code pillow",
-      count: 1,
-      whatILike: "Eat. Sleep. Code. Repeat :)"
-    }
-  ];
+ import { myCollection } from "./collection.js"
 // console.log(myCollection);
 
 // If there's only one of the item in your collection, it should log "I have a name. Here's what I like about it: whatILike". 
 // If you have more than one of it, the message should log "I have count names. Here's what I like about them: whatILike".
+let i = 0;
 
 function describeItem(item){
   if (item.count === 1){ 
