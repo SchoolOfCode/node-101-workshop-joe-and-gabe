39d2e5a857f5d3628d80e771380c4dd5e3732218
jafcoder <joefarr113@gmail.com>
2021-12-06 17:01:25 +0000

    2c updated for more than 1

--- index.js
+++ index.js
@@ -25,8 +25,8 @@ function describeItem(item){
     console.log("I have a" + item.name + ". Here's what I like about it:" + item.whatILike + ".")
   } 
   if (item.count > 1){
-      console.log("I have a " + item.name + ". Here's what I like about it: " + item.whatILike + ".")
+      console.log("I have " + item.count + " " + item.name +"s" + ". Here's what I like about them: " + item.whatILike + ".")
     }
   }
   
-describeItem(myCollection[2])
+describeItem(myCollection[1])
